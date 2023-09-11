1ab44793ae2e60578c07774e2dc77d683e642049
JeanDeveloper <jecachusa.1996@gmail.com>
2023-09-11 08:37:25 -0500

    actualizando ando

--- lib/driver/data/repositories/firebase_auth_repository.dart
+++ lib/driver/data/repositories/firebase_auth_repository.dart
@@ -3,7 +3,11 @@
 // import 'package:firebase_auth_platform_interface/src/providers/phone_auth.dart';
 // import 'package:firebase_auth_platform_interface/src/firebase_auth_exception.dart';
 // import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:io';
+
+import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:taxi/app/core/errors/failure.dart';
 import 'package:taxi/driver/data/datasource/auth_datasource.dart';
 import 'package:taxi/driver/domain/repositories/iauth_repository.dart';
 
@@ -36,45 +40,12 @@ class FirebaseAuthRepository extends IAuthRepository{
     UserCredential? user = await authDataSource.verifyOTP(codeNumber, verification);
     return user;
   }
-
-
-  // @override
-  // Future<void> verifyPhoneNumber(
-  //   {
-  //     required String phone, 
-  //     required Function(PhoneAuthCredential p1) onCompleted, 
-  //     required Function(FirebaseAuthException p1) onFailed, 
-  //     required Function(String p1, int? p2) onCodeSent, 
-  //     required Function(String p1) onTimeout
-  //   }
-  // ) async {
-
-  //   await authDataSource.verifyPhoneNumber(
-  //     phone, 
-  //     onCompleted: onCompleted, 
-  //     onFailed: onFailed, 
-  //     onCodeSent: onCodeSent, 
-  //     onTimeout: onTimeout
-  //   );
-
-  // }
   
-  // @override
-  // Future<void> verifyPhoneNumber(String phoneNumber) async  {
-    
-  //   await authDataSource.verifyPhoneNumber(phoneNumber);
-
-  //   // on AuthException catch(auth){
-  //   //   return Left(AuthFailure(message: auth.message ));
-  //   // } 
-
-  //   // on ServerException {
-  //   //   return Left(ServerFailure(message: "Ha ocurrido un problema en el servidor"));
-  //   // } 
-
-  // }
-
-
+  @override
+  Future<Either<Failure, String?>> uploadFile(File file) {
+    // TODO: implement uploadFile
+    throw UnimplementedError();
+  }
 
 
 }
